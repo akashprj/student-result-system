150f7c4c42ee9b27693236bf3d646efbbd7d2c36
Akash Kumar Prajapati <91784572+akashprj@users.noreply.github.com>
2022-12-14 17:48:04 +0530

    Add files via upload

---
+++ src/ManagementSystem/Connection.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ManagementSystem;
+
+/**
+ *
+ * @author 91737
+ */
+class Connection {
+
+    Statement createStatement(Class<ResultSet> aClass) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    Statement createStatement() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
